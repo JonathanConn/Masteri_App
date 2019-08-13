7a322eb161b73d6ae6d6e3f4edbefdf06832c021
Jonathan <Jonathanconn29@gmail.com>
2019-08-12 21:50:22 -0400

    changed table structure

--- lib/data/score.dart
+++ lib/data/score.dart
@@ -2,24 +2,30 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ScorePage extends StatelessWidget {
+
  @override
  Widget build(BuildContext context) {
    return Container(
      padding: EdgeInsets.all(20),
      child: Table(
+       defaultColumnWidth: FlexColumnWidth(5),
        border: TableBorder.all(width: 1.0, color: Colors.black),
        children: [
-        TableRow(
-          children:[
-            new Text("test", textAlign: TextAlign.center)
-          ]
-        ),
-        TableRow(
-          children: [
-            new Text("test", textAlign: TextAlign.center)
-          ]
-        ),
-      ]),
+        _buildRow(1, "Jack", 500),
+        _buildRow(2, "Jack", 500),
+        _buildRow(3, "Jack", 500),
+      ]
+    ),
    );
  }
+
+ TableRow _buildRow(int place, String name, double score){
+   return TableRow(
+    children:[   
+      Text(place.toString() + ".\t " + name, textAlign: TextAlign.center),
+      Text(score.toString(), textAlign: TextAlign.center)
+    ],
+   );
+ }
+
 }
