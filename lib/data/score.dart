6f1b584c3a1df28d37ac63e8e2573d5ee486fa95
Jonathan <Jonathanconn29@gmail.com>
2019-08-12 22:09:07 -0400

    removed table and created listview linked to live firestore, currently working

--- lib/data/score.dart
+++ lib/data/score.dart
@@ -5,26 +5,37 @@ class ScorePage extends StatelessWidget {
 
  @override
  Widget build(BuildContext context) {
-   return Container(
-     padding: EdgeInsets.all(20),
-     child: Table(
-       defaultColumnWidth: FlexColumnWidth(5),
-       border: TableBorder.all(width: 1.0, color: Colors.black),
-       children: [
-        _buildRow(1, "Jack", 500),
-        _buildRow(2, "Jack", 500),
-        _buildRow(3, "Jack", 500),
-      ]
-    ),
+   return Scaffold(
+     body: StreamBuilder(
+       stream: Firestore.instance.collection('scores').snapshots(),
+       builder: (context, snapshot){
+         if(!snapshot.hasData) return const Text('Loading...');
+         return ListView.builder(
+           itemExtent: 80,
+           itemCount: snapshot.data.documents.length,
+           itemBuilder: (context, index) => 
+           _buildListItem(context, snapshot.data.documents[index]),
+         );
+       },
+     ),
    );
  }
 
- TableRow _buildRow(int place, String name, double score){
-   return TableRow(
-    children:[   
-      Text(place.toString() + ".\t " + name, textAlign: TextAlign.center),
-      Text(score.toString(), textAlign: TextAlign.center)
-    ],
+ Widget _buildListItem(BuildContext context, DocumentSnapshot document){
+   return ListTile(
+     title: Row(children: [
+       Expanded(child: Text(
+         document['name'],
+       ),
+       ),
+       Container(
+         padding: EdgeInsets.all(5),
+         child: Text(
+           document['time'].toString(),
+         ),
+       ),
+     ],
+     ),
    );
  }
 
